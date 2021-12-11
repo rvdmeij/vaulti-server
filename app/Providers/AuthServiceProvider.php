40825d21054f70f1509fbae259b49a99f695db2f
Roy van der Meij <rvdmeij@2li.nl>
2021-12-11 02:08:01 +0100

    Removed non used Gate import

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 
 use App\Models\Vault;
 use App\Policies\VaultPolicy;
